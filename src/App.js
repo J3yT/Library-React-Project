ad099de8d8ed8a2e74227843a97c040ae5cfa15f
Jason Tavera <taverajason7@gmail.com>
2023-08-22 20:00:30 -0500

    installed fontawesome dependencies and imported bars onto navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Nav from "./components/Nav";
+
+
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App">
+    <Nav />
+  </div>;
 }
 
 export default App;
